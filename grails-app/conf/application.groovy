b2d97cdebe35be785ec49e173d08e68d2cde0ac5
William Woodman <William Woodman>
2017-02-25 15:48:18 +0000

    updated with spring correected user/group/role model - baseline to transfer to git hub

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -5,12 +5,14 @@
 //added to avoid login screens for dbconsole.  if you add /** at end it works
 grails.plugin.springsecurity.rejectIfNoRule = true
 //grails.plugin.springsecurity.fii.rejectPublicInvocations = false
+grails.plugin.springsecurity.securityConfigType = "Annotation"
 
 // Added by the Spring Security Core plugin:
 grails.plugin.springsecurity.userLookup.userDomainClassName = 'org.softwood.security.User'
-grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'org.softwood.security.UserToRole'
+grails.plugin.springsecurity.userLookup.authoritiesPropertyName = 'authorities'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'org.softwood.security.UserToUserGroup'
 grails.plugin.springsecurity.authority.className = 'org.softwood.security.Role'
-grails.plugin.springsecurity.authority.groupAuthorityNameField = 'authorities'
+grails.plugin.springsecurity.authority.groupAuthorityNameField = 'authority'//'authorities'
 grails.plugin.springsecurity.useRoleGroups = true
 grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	[pattern: '/',               access: ['permitAll']],
@@ -20,6 +22,8 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	[pattern: '/shutdown',       access: ['permitAll']],
 	[pattern: '/dbconsole/**',   access: ['permitAll']],    //added
 	[pattern: '/console/**',     access: ['permitAll']],	//added
+		[pattern: '/secureTest/secure',    access: ['ROLE_ADMIN']],
+        [pattern: '/secureTest/index',     access: ['permitAll']],
 	[pattern: '/assets/**',      access: ['permitAll']],
 	[pattern: '/**/js/**',       access: ['permitAll']],
 	[pattern: '/**/css/**',      access: ['permitAll']],
