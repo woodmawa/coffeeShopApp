2b6d79885a4b374fae13df2f60b9e4be876b6bb9
William Woodman <William Woodman>
2017-02-25 16:57:13 +0000

    added secureTestController

---
+++ grails-app/controllers/org/softwood/SecureTestController.groovy
@@ -0,0 +1,16 @@
+package org.softwood
+
+import grails.plugin.springsecurity.annotation.Secured
+
+class SecureTestController {
+
+    def index() {
+        render "hello Will you passed the permit_any"
+    }
+
+    @Secured ('ROLE_ADMIN')
+    def secure () {
+        render "hello Will you passed the ROLE_ADMIN"
+
+    }
+}
